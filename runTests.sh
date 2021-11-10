6de985dccf95dc36643f5c7cfa334e3e607d874b
Eric <erjseals@gmail.com>
2021-11-09 21:54:49 -0600

    minor changes to bash script

--- runTests.sh
+++ runTests.sh
@@ -2,13 +2,12 @@
 
 for limit in {0..512..16}
 do
-  touch "prof-${limit}-10.txt"
   for throttle in {0..32..4}
   do
     echo $limit > /sys/kernel/debug/throttle/limit
     echo $throttle > /sys/kernel/debug/throttle/throttle
 
-    /usr/local/cuda/bin/nvprof -o ~/dev/linux-kernerl/profile/prof-${limit}-${throttle}.nvvp 
+    /usr/local/cuda/bin/nvprof -o ~/dev/linux-kernerl/profile/prof-${limit}-${throttle}.nvvp ~/Documents/NVIDIA_CUDA-10.2_Samples/0_Simple/matrixMul/./matrixMul
   done
 done
 
