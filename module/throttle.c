9658e467e5f4d7b891f3a0c024bcef495885dfcc
Eric <erjseals@gmail.com>
2021-11-09 21:28:43 -0600

    Actually usable by debugfs

--- module/throttle.c
+++ module/throttle.c
@@ -28,6 +28,8 @@ MODULE_AUTHOR("Eric Seals <ericseals@ku.edu>");
 static struct dentry *throttle_dir = 0;
 
 static u32 throttleAmount = 0;
+static u32 throttleLimit  = 0;
+static u32 limitAmount    = 0;
 
 /**************************************************************************
  * Functions 
@@ -35,52 +37,46 @@ static u32 throttleAmount = 0;
 
 static int set_throttle_op(void *data, u64 value)
 {
+  u32 bitWise = 0;
+
+  // Define Throttling amount
+  // Bits 20:16 set throttling amount when limit is exceeded
+  // Bits 4:0 set throttling for when limit not exceeded
+  void __iomem *io = ioremap(MC_EMEM_ARB_RING1_THROTTLE_0, 32);
+
+  bitWise = (value << 16) | value;
+  iowrite32( bitWise , io);
+
+  throttleAmount = value;
+  return 0;
+}
+
+static int set_limit_op(void *data, u64 value)
+{
   // Set Request Limit (If outstanding requests exceed, throttling initiates) 
   // 31st bit LOW - don't limit requests, we want them to exceed the arbitration limit 
   //
   // Bits 8:0 sets the request limit
   u32 bitWise = ~(1 << 31);
-  u32 limit = 0x00000001;
-
-  u64 throttle = value;
 
   void __iomem *io = ioremap(MC_EMEM_ARB_OUTSTANDING_REQ_0, 32);
-
   // Clear the low 8 bits
   // to be or-ed with limit
   bitWise = bitWise & 0x11111E00;
-  iowrite32( ( (ioread32(io) & bitWise) | limit ) , io);
-
-  // Define Throttling amount
-  // Bits 20:16 set throttling amount when limit is exceeded
-  // Bits 4:0 set throttling for when limit not exceeded
-  bitWise = (throttle << 16) | throttle;
-  io = ioremap(MC_EMEM_ARB_RING1_THROTTLE_0, 32);
-  iowrite32( bitWise , io);
+  iowrite32( ( (ioread32(io) & bitWise) | value ) , io);
 
   // Enable Ring1 Arbitration
   bitWise = 0x80008000;
   io = ioremap(MC_EMEM_ARB_RING0_THROTTLE_MASK_0, 32);
   iowrite32( bitWise , io);
 
-
-  // Enable throttling at Ring2 output
-  // Misnamed RING3_THROTTLE in the TRM
-  bitWise = 0xFFFFFE01;
-  io = ioremap(MC_EMEM_ARB_OUTSTANDING_REQ_RING3_0, 32);
-  iowrite32( (ioread32(io) & bitWise) , io);
-
-  // Define Throttling amount
-  // Bits 4:0 set throttling amount when limit is exceeded
-  bitWise = 0;
-  io = ioremap(MC_EMEM_ARB_RING3_THROTTLE_0, 32);
-  iowrite32( (ioread32(io) | bitWise) , io);
-
-  throttleAmount = value;
+  throttleLimit = value;
   return 0;
+
 }
 
-DEFINE_SIMPLE_ATTRIBUTE(add_fops, NULL, set_throttle_op, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(throttle_fops, NULL, set_throttle_op, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(limit_fops, NULL, set_limit_op, "%llu\n");
 
 static int throttle_init_debugfs(void)
 {
@@ -94,16 +90,27 @@ static int throttle_init_debugfs(void)
   }
 
   junk = debugfs_create_file(
-          "set",
+          "throttle",
           0444,
           throttle_dir,
           NULL,
-          &add_fops);
+          &throttle_fops);
   if (!junk) {
-    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/throttle/set\n");
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/throttle/throttle\n");
   }
 
-  debugfs_create_u32("limit", 0444, throttle_dir, &throttleAmount);
+  junk = debugfs_create_file(
+          "limit",
+          0444,
+          throttle_dir,
+          NULL,
+          &limit_fops);
+  if (!junk) {
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/throttle/limit\n");
+  }
+
+  debugfs_create_u32("throttleLimit", 0444, throttle_dir, &throttleLimit);
+  debugfs_create_u32("throttleAmount", 0444, throttle_dir, &throttleAmount);
 
   return 0;
 }
