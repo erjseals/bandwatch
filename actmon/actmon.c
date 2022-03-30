e63efd5b873085956211c83e7f92b56278a6f459
Eric <erjseals@gmail.com>
2022-03-30 13:52:00 -0500

    Push merged kernel modules with rough MVP of bandwidth reg at 1ms period

--- actmon/actmon.c
+++ actmon/actmon.c
@@ -5,6 +5,7 @@
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
 #include <linux/slab.h>
+#include <linux/timer.h>
 
 // The GPL license is required for the module to load. 
 // Without it, insmod reports unknown symbols in the module.
@@ -42,13 +43,36 @@ static struct dentry *actmon_dir = 0;
 
 static u32 EnableACTMON = 0;
 static u32 MCALL_AVG = 0;
-/*
-static u32 MCCPU_AVG = 0;
-*/
+
+// Timer Values
+// 1ms period
+int g_time_interval = 1000;
+struct timer_list g_timer;
 
 /**************************************************************************
  * Functions 
  **************************************************************************/
+
+void _TimerHandler(unsigned long data)
+{
+  /* 1. Check Current MC Bandwidth */
+
+
+  /* 2.a. If above threshold, increase throttle */
+
+  /* 2.b. If below threshold, decrease throttle */
+
+
+
+
+
+
+
+  /* Rewind the Timer */
+  mod_timer(&g_timer, jiffies + msecs_to_jiffies(g_time_interval));
+
+  printk(KERN_INFO "Timer reset by Handler\n");
+}
 
 // value is unused, clean this up later
 static int set_actmon_op(void *data, u64 value)
@@ -86,22 +110,6 @@ static int set_actmon_op(void *data, u64 value)
   bitWise = (1 << 31);
   iowrite32( (ioread32(io) | bitWise) , io);
 
-  /*
-  // MCCPU (Currently non-functional)
-  //
-  // Initialize the AVG Count to 0
-  bitWise = 0;
-  io = ioremap(ACTMON_ADDRESS + ACTMON_MCCPU_INIT_AVG_0, 32);
-  iowrite32( bitWise , io);
-
-  // Enable MC Activity Monitor
-  // Write 1 to bit 31
-  io = ioremap(ACTMON_ADDRESS + ACTMON_MCCPU_CTRL_0, 32);
-
-  bitWise = (1 << 31);
-  iowrite32( (ioread32(io) | bitWise) , io);
-  */
-
   EnableACTMON = bitWise;
   return 0;
 }
@@ -116,23 +124,8 @@ static int set_mcall_count_op(void *data, u64 value)
   return 0;
 }
 
-/*
-static int set_mccpu_count_op(void *data, u64 value)
-{
-  // Read the number of avg cycles 
-  // This will be placed in file mcall_count
-  void __iomem *io = ioremap(ACTMON_ADDRESS + ACTMON_MCCPU_AVG_COUNT_0, 32);
-
-  MCCPU_AVG = ioread32(io);
-  return 0;
-}
-*/
-
 DEFINE_SIMPLE_ATTRIBUTE(actmon_fops, NULL, set_actmon_op, "%llu\n");
 DEFINE_SIMPLE_ATTRIBUTE(mcall_count_fops, NULL, set_mcall_count_op, "%llu\n");
-/*
-DEFINE_SIMPLE_ATTRIBUTE(mccpu_count_fops, NULL, set_mccpu_count_op, "%llu\n");
-*/
 
 static int actmon_init_debugfs(void)
 {
@@ -165,41 +158,32 @@ static int actmon_init_debugfs(void)
     printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/actmon/mcall_count\n");
   }
 
-  /*
-  junk = debugfs_create_file(
-          "mccpu_count",
-          0444,
-          actmon_dir,
-          NULL,
-          &mccpu_count_fops);
-  if (!junk) {
-    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/actmon/mccpu_count\n");
-  }
-  */
-
   debugfs_create_u32("EnableACTMON", 0444, actmon_dir, &EnableACTMON);
   debugfs_create_u32("MCALL_AVG", 0444, actmon_dir, &MCALL_AVG);
-  /*
-  debugfs_create_u32("MCCPU_AVG", 0444, actmon_dir, &MCCPU_AVG);
-  */
 
   return 0;
 }
 
 
 static int __init actmon_init(void) {
-  printk(KERN_INFO "ACTMON module has been loaded\n");
-
   actmon_init_debugfs();
 
+  /* Start the periodic timer */
+  setup_timer(&g_timer, _TimerHandler, 0);
+  mod_timer(&g_timer, jiffies + msecs_to_jiffies(g_time_interval));
+
+  printk(KERN_INFO "ACTMON module has been loaded\n");
   return 0;
 }
 
 static void __exit actmon_exit(void) {
-  printk(KERN_INFO "ACTMON module has been unloaded\n");
-
   /* remove debugfs entries */
   debugfs_remove_recursive(actmon_dir);
+
+  /* Delete the timer */
+  del_timer(&g_timer);
+
+  printk(KERN_INFO "ACTMON module has been unloaded\n");
 }
 
 module_init(actmon_init);
---
+++ module/bandWatch.c
@@ -0,0 +1,295 @@
+#include <asm/io.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/debugfs.h>
+#include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/timer.h>
+
+// The GPL license is required for the module to load. 
+// Without it, insmod reports unknown symbols in the module.
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Eric Seals <ericseals@ku.edu>");
+
+/**************************************************************************
+ * Throttle Addresses 
+ **************************************************************************/
+
+// Base Address
+#define MC_ADDRESS 0x70019000
+#define MC_EMEM_ARB_OUTSTANDING_REQ_0       0x70019094
+#define MC_EMEM_ARB_RING1_THROTTLE_0        0x700190e0
+#define MC_EMEM_ARB_RING3_THROTTLE_0        0x700190e4
+// Disable bit 31 for ring1 arbitration
+#define MC_EMEM_ARB_RING0_THROTTLE_MASK_0   0x700196bc
+
+#define MC_EMEM_ARB_OUTSTANDING_REQ_RING3_0 0x7001966c
+
+/**************************************************************************
+ * Activity Monitor Addresses 
+ **************************************************************************/
+
+// Base Address
+#define ACTMON_ADDRESS                0x6000c800
+
+// Offsets
+
+// Global Enable
+#define ACTMON_GLB_STATUS_0		        0x0
+#define ACTMON_GLB_PERIOD_CTRL_0      0x4
+
+// All Memory Controller Traffic
+#define ACTMON_MCALL_CTRL_0		        0x1c0
+#define ACTMON_MCALL_INIT_AVG_0       0x1cc
+#define ACTMON_MCALL_COUNT_0          0x1dc
+#define ACTMON_MCALL_AVG_COUNT_0	    0x1e0
+
+// Only CPU Memory Controller Traffic
+#define ACTMON_MCCPU_CTRL_0		        0x200
+#define ACTMON_MCCPU_INIT_AVG_0		    0x20c
+#define ACTMON_MCCPU_COUNT_0		      0x21c
+#define ACTMON_MCCPU_AVG_COUNT_0	    0x220
+
+
+/**************************************************************************
+ * Global Variables
+ **************************************************************************/
+
+static struct dentry *bandWatch_dir = 0;
+
+static u32 enableActmon = 0;
+static u32 MCALL_AVG = 0;
+
+static u32 throttleAmount = 0;
+static u32 throttleLimit  = 0;
+
+// Timer Values
+// 1ms period
+int g_time_interval = 1000;
+struct timer_list g_timer;
+
+/**************************************************************************
+ * Functions 
+ **************************************************************************/
+
+static int set_throttle_op(void *data, u64 value)
+{
+  u32 bitWise = 0;
+
+  // Define Throttling amount
+  // Bits 20:16 set throttling amount when limit is exceeded
+  // Bits 4:0 set throttling for when limit not exceeded
+  void __iomem *io = ioremap(MC_EMEM_ARB_RING1_THROTTLE_0, 32);
+
+  bitWise = (value << 16) | value;
+  iowrite32( bitWise , io);
+
+  throttleAmount = value;
+  return 0;
+}
+
+static int set_limit_op(void *data, u64 value)
+{
+  // Set Request Limit (If outstanding requests exceed, throttling initiates) 
+  // 31st bit LOW - don't limit requests, we want them to exceed the arbitration limit 
+  //
+  // Bits 8:0 sets the request limit
+  u32 bitWise = ~(1 << 31);
+
+  void __iomem *io = ioremap(MC_EMEM_ARB_OUTSTANDING_REQ_0, 32);
+  // Clear the low 8 bits
+  // to be or-ed with limit
+  bitWise = bitWise & 0x11111E00;
+  iowrite32( ( (ioread32(io) & bitWise) | value ) , io);
+
+  // Enable Ring1 Arbitration
+  bitWise = 0x80008000;
+  io = ioremap(MC_EMEM_ARB_RING0_THROTTLE_MASK_0, 32);
+  iowrite32( bitWise , io);
+
+  throttleLimit = value;
+  return 0;
+
+}
+
+int set_throttle(u64 value)
+{
+  u32 bitWise = 0;
+
+  // Define Throttling amount
+  // Bits 20:16 set throttling amount when limit is exceeded
+  // Bits 4:0 set throttling for when limit not exceeded
+  void __iomem *io = ioremap(MC_EMEM_ARB_RING1_THROTTLE_0, 32);
+
+  bitWise = (value << 16) | value;
+  iowrite32( bitWise , io);
+
+  throttleAmount = value;
+  return 0;
+}
+
+void _TimerHandler(struct timer_list *timer)
+{
+  // Make this globally constant later
+  int MAX_BANDWIDTH = 12500;
+  /* 1. Check Current MC Bandwidth */
+  /* 2.a. If above threshold, increase throttle */
+
+  if (MCALL_AVG > MAX_BANDWIDTH) {
+    if (throttleAmount < 31) {
+      set_throttle(throttleAmount + 1);
+    }
+  }
+
+  /* 2.b. If below threshold, decrease throttle */
+  if (MCALL_AVG <= MAX_BANDWIDTH) {
+    if (throttleAmount > 0) {
+      set_throttle(throttleAmount - 1);
+    }
+  }
+
+  /* Rewind the Timer */
+  mod_timer(&g_timer, jiffies + msecs_to_jiffies(g_time_interval));
+
+  printk(KERN_INFO "Timer reset by Handler\n");
+}
+
+// value is unused, clean this up later
+static int set_actmon_op(void *data, u64 value)
+{
+  // Initialize Global Enable
+  // MCALL bit 9
+  // MCCPU bit 8 
+  u32 bitWise = (1 << 9);
+
+  void __iomem *io = ioremap(ACTMON_ADDRESS + ACTMON_GLB_STATUS_0, 32);
+  iowrite32( (ioread32(io) | bitWise) , io);
+
+  // Initialize Global Period
+  // Writing 1/0 to bit 8 sets period time base in usec/msec
+  // Writing n to bits 7:0 creates a n+1 usec/msec sampling period
+
+  // Setting for 10 usec
+  bitWise = (1 << 8) | (0x9);
+  //bitWise = 0;
+
+  io = ioremap(ACTMON_ADDRESS + ACTMON_GLB_PERIOD_CTRL_0, 32);
+  iowrite32( bitWise , io);
+
+  // MCALL
+  //
+  // Initialize the AVG Count to 0
+  bitWise = 0;
+  io = ioremap(ACTMON_ADDRESS + ACTMON_MCALL_INIT_AVG_0, 32);
+  iowrite32( bitWise , io);
+
+  // Enable MC Activity Monitor
+  // Write 1 to bit 31
+  io = ioremap(ACTMON_ADDRESS + ACTMON_MCALL_CTRL_0, 32);
+
+  bitWise = (1 << 31);
+  iowrite32( (ioread32(io) | bitWise) , io);
+
+  enableActmon = bitWise;
+  return 0;
+}
+
+static int set_mcall_count_op(void *data, u64 value)
+{
+  // Read the number of avg cycles 
+  // This will be placed in file mcall_count
+  void __iomem *io = ioremap(ACTMON_ADDRESS + ACTMON_MCALL_AVG_COUNT_0, 32);
+
+  MCALL_AVG = ioread32(io);
+  return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(actmon_fops, NULL, set_actmon_op, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(mcall_count_fops, NULL, set_mcall_count_op, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(throttle_fops, NULL, set_throttle_op, "%llu\n");
+DEFINE_SIMPLE_ATTRIBUTE(limit_fops, NULL, set_limit_op, "%llu\n");
+
+static int bandWatch_init_debugfs(void)
+{
+  struct dentry *junk;
+
+  bandWatch_dir = debugfs_create_dir("bandWatch", NULL);
+  if (!bandWatch_dir) {
+    // Abort module load
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/bandWatch\n");
+    return -1;
+  }
+
+  junk = debugfs_create_file(
+          "throttle",
+          0444,
+          bandWatch_dir,
+          NULL,
+          &throttle_fops);
+  if (!junk) {
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/bandWatch/throttle\n");
+  }
+
+  junk = debugfs_create_file(
+          "limit",
+          0444,
+          bandWatch_dir,
+          NULL,
+          &limit_fops);
+  if (!junk) {
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/bandWatch/limit\n");
+  }
+
+  junk = debugfs_create_file(
+          "actmon",
+          0444,
+          bandWatch_dir,
+          NULL,
+          &actmon_fops);
+  if (!junk) {
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/bandWatch/actmon\n");
+  }
+  
+  junk = debugfs_create_file(
+          "mcall_count",
+          0444,
+          bandWatch_dir,
+          NULL,
+          &mcall_count_fops);
+  if (!junk) {
+    printk(KERN_ALERT "debugfs: failed to create /sys/kernel/debug/bandWatch/mcall_count\n");
+  }
+
+  debugfs_create_u32("enableActmon", 0444, bandWatch_dir, &enableActmon);
+  debugfs_create_u32("MCALL_AVG", 0444, bandWatch_dir, &MCALL_AVG);
+  debugfs_create_u32("throttleLimit", 0444, bandWatch_dir, &throttleLimit);
+  debugfs_create_u32("throttleAmount", 0444, bandWatch_dir, &throttleAmount);
+
+  return 0;
+}
+
+
+static int __init bandWatch_init(void) {
+  bandWatch_init_debugfs();
+
+  /* Start the periodic timer */
+  timer_setup(&g_timer, _TimerHandler, 0);
+  mod_timer(&g_timer, jiffies + msecs_to_jiffies(g_time_interval));
+
+  printk(KERN_INFO "bandWatch module has been loaded\n");
+  return 0;
+}
+
+static void __exit bandWatch_exit(void) {
+  /* remove debugfs entries */
+  debugfs_remove_recursive(bandWatch_dir);
+
+  /* Delete the timer */
+  del_timer(&g_timer);
+
+  printk(KERN_INFO "bandWatch module has been unloaded\n");
+}
+
+module_init(bandWatch_init);
+module_exit(bandWatch_exit);
