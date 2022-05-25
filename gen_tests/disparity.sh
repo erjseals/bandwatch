d4c304052b380f6126e7f70340d35086e2001c24
Eric Seals <erjseals@gmail.com>
2022-05-25 15:54:38 -0500

    Re-sync tests

--- gen_tests/disparity.sh
+++ gen_tests/disparity.sh
@@ -13,7 +13,7 @@ echo "Building Memguard"
 echo "$TEST in Isolation"
 INTERF="Isolated"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 sudo echo 0 > /sys/kernel/debug/tracing/trace
 
 taskset -c 0 ../benchmarks/sd-vbs/benchmarks/disparity/data/fullhd/disparity ../benchmarks/sd-vbs/benchmarks/disparity/data/fullhd/. | grep "Cycles elapsed" > cycles_${TEST}${INTERF}.txt & PID_TO_WAIT=$!
@@ -30,7 +30,7 @@ python3 splitftrace.py trace_${TEST}${INTERF}.txt
 INTERF="hesocMemcpy"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 sudo taskset -c 0 ../benchmarks/hesoc-mark/cuda/cudainterf -d 102400 -i 20000 -m memcpy & PID_TO_KILL0=$!
 
@@ -54,7 +54,7 @@ python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
 INTERF="hesocMemset"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 sudo taskset -c 0 ../benchmarks/hesoc-mark/cuda/cudainterf -d 102400 -i 20000 -m memset & PID_TO_KILL0=$!
 
@@ -78,7 +78,7 @@ python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
 INTERF="bandwidthRead"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 for c in 1 2 3; do bandwidth -c $c -t 1000 & done
 
@@ -101,7 +101,7 @@ python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
 INTERF="bandwidthWrite"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 for c in 1 2 3; do bandwidth -a write -c $c -t 1000 & done
 
@@ -124,7 +124,7 @@ python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
 INTERF="bandwidthRead_hesocMemcpy"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 for c in 1 2 3; do bandwidth -c $c -t 1000 & done
 sudo taskset -c 0 ../benchmarks/hesoc-mark/cuda/cudainterf -d 102400 -i 20000 -m memcpy & PID_TO_KILL0=$!
@@ -150,7 +150,7 @@ python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
 INTERF="bandwidthWrite_hesocMemset"
 echo "$TEST against $INTERF"
 
-sudo insmod ../patched_memguard/memguard.ko
+sudo insmod ../patched_memguard/memguard.ko g_hw_counter_id=0x17
 
 for c in 1 2 3; do bandwidth -a write -c $c -t 1000 & done 
 sudo taskset -c 0 ../benchmarks/hesoc-mark/cuda/cudainterf -d 102400 -i 20000 -m memset & PID_TO_KILL0=$!
