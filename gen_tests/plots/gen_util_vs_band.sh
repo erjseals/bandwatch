639fd5e0ad446e0719f879cff63b0e34d586e756
Eric <erjseals@gmail.com>
2022-05-25 22:20:27 -0500

    Numbers are bad? Resync and re-conduct test

--- gen_tests/plots/gen_util_vs_band.sh
+++ gen_tests/plots/gen_util_vs_band.sh
@@ -1,1 +1,13 @@
-gnuplot -c gnuplot.scr ../tests/parsed_trace_DisparityIsolated.txt > util_vs_band/DisparityIsolated.pdf
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_DisparityIsolated.txt > util_vs_band/DisparityIsolated.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_bandwidthRead_hesocMemcpy.txt > util_vs_band/Disparity_vs_bandwidthRead_hesocMemcpy.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_bandwidthRead.txt > util_vs_band/Disparity_vs_bandwidthRead.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_bandwidthWrite_hesocMemset.txt > util_vs_band/Disparity_vs_bandwidthWrite_hesocMemset.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_bandwidthWrite.txt > util_vs_band/Disparity_vs_bandwidthWrite.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_hesocMemcpy.txt > util_vs_band/Disparity_vs_hesocMemcpy.pdf
+
+gnuplot -c gnuplot.scr ../tests_uSec/parsed_trace_Disparity_vs_hesocMemset.txt > util_vs_band/Disparity_vs_hesocMemset.pdf
