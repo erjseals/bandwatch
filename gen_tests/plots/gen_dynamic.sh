6a31e3c8b8baf3e70bdb5318e1ca1543bccfafbd
Eric <erjseals@gmail.com>
2022-05-30 13:50:39 -0500

    zoomed plots

--- gen_tests/plots/gen_dynamic.sh
+++ gen_tests/plots/gen_dynamic.sh
@@ -3,3 +3,5 @@
 gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/parsed_trace_dynamic_memset.txt > dynamicTests/memset.pdf
 gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/parsed_trace_dynamic_memcpy.txt > dynamicTests/memcpy.pdf
 
+gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/parsed_trace_dynamic_memset.txt > dynamicTests/memset_zoomed.pdf
+gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/parsed_trace_dynamic_memcpy.txt > dynamicTests/memcpy_zoomed.pdf
