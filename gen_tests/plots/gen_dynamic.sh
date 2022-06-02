b0b9649818fcdb51d37da8f0d566a740c360019a
Eric <erjseals@gmail.com>
2022-06-02 16:54:10 -0500

    Test algorithm against all baseline evaluation interference

--- gen_tests/plots/gen_dynamic.sh
+++ gen_tests/plots/gen_dynamic.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash 
 
+TEST="disparity"
+
 mkdir -p plots_dynamic
 
-gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/res/disparity/memset/parsed_trace_dynamic_memset.txt > plots_dynamic/memset.pdf
-#gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/res/disparity/memcpy/parsed_trace_dynamic_memcpy.txt > plots_dynamic/memcpy.pdf
+gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/res/${TEST}/memset/parsed_trace_dynamic_memset.txt > plots_dynamic/memset.pdf
+gnuplot -c gnuplot_dynamic.scr ../tests_dynamic/res/${TEST}/memcpy/parsed_trace_dynamic_memcpy.txt > plots_dynamic/memcpy.pdf
 
-gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/res/disparity/memset/parsed_trace_dynamic_memset.txt > plots_dynamic/memset_zoomed.pdf
-#gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/res/disparity/memcpy/parsed_trace_dynamic_memcpy.txt > plots_dynamic/memcpy_zoomed.pdf
+gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/res/${TEST}/memset/parsed_trace_dynamic_memset.txt > plots_dynamic/memset_zoomed.pdf
+gnuplot -c gnuplot_dynamic_zoomed.scr ../tests_dynamic/res/${TEST}/memcpy/parsed_trace_dynamic_memcpy.txt > plots_dynamic/memcpy_zoomed.pdf
