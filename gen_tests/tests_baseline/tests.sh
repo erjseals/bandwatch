11f958532d79ce0b73a1c7883f226b0e9b25f540
Eric Seals <erjseals@gmail.com>
2022-06-05 18:45:28 -0500

    Tests reran to validate numbers

--- gen_tests/tests_baseline/tests.sh
+++ gen_tests/tests_baseline/tests.sh
@@ -1,21 +1,59 @@
 #! /usr/bin/env bash 
 
-declare -a LOOP=(0)
+declare -a LOOP=(5)
 
 sysctl -w kernel.sched_rt_runtime_us=-1 
 
 TEST="tracking"
 SIZE="250000"
 
-isolated=12
+isolated=121
 memcpy=12
 memset=12
-bandwidth=1
-bandwidth_heavy=1
+bandwidth=12
+bandwidth_heavy=12
 benchmarks=5
+benchmarks_read=1
+loop=2
 
 sudo insmod ../../patched_memguard/memguard.ko g_hw_counter_id=0x17
 sleep 2
+
+if [[ $benchmarks_read == 1 ]]
+then
+  mkdir -p res/bandwidth_rw
+  sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+  bandwidth -c 0 -t 1000 >> bw_read.txt & 
+
+  sleep 5
+    
+  sudo cat /sys/kernel/debug/tracing/trace > trace_bandwidth_read.txt
+
+  sudo killall -2 bandwidth > /dev/null 2>&1
+  sleep 1
+  sudo killall -9 bandwidth > /dev/null 2>&1
+
+  sleep 5
+
+  mkdir -p res/bandwidth_rw
+  sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+  bandwidth -a write -c 0 -t 1000 >> bw_write.txt & 
+
+  sleep 5
+    
+  sudo cat /sys/kernel/debug/tracing/trace > trace_bandwidth_write.txt
+
+  sudo killall -2 bandwidth > /dev/null 2>&1
+  sleep 1
+  sudo killall -9 bandwidth > /dev/null 2>&1
+
+  python3 splitftrace.py trace_bandwidth_read.txt
+  python3 splitftrace.py trace_bandwidth_write.txt
+
+  mv *.txt res/bandwidth_rw
+fi
 
 if [[ $benchmarks == 1 ]]
 then
@@ -74,240 +112,242 @@ then
   sleep 20
 fi
 
+if [[ $loop == 1 ]] 
+then
+  for i in "${LOOP[@]}"
+  do
+    if [[ $i == 0 ]]
+    then
+      TEST="disparity"
+    fi
+    if [[ $i == 1 ]]
+    then
+      TEST="mser"
+    fi
+    if [[ $i == 2 ]]
+    then
+      TEST="sift"
+    fi
+    if [[ $i == 3 ]]
+    then
+      TEST="texture_synthesis"
+    fi
+    if [[ $i == 4 ]]
+    then
+      TEST="tracking"
+    fi
 
-for i in "${LOOP[@]}"
-do
-  if [[ $i == 0 ]]
-  then
-    TEST="disparity"
-  fi
-  if [[ $i == 1 ]]
-  then
-    TEST="mser"
-  fi
-  if [[ $i == 2 ]]
-  then
-    TEST="sift"
-  fi
-  if [[ $i == 3 ]]
-  then
-    TEST="texture_synthesis"
-  fi
-  if [[ $i == 4 ]]
-  then
-    TEST="tracking"
-  fi
+    mkdir -p res/${TEST}
 
-  mkdir -p res/${TEST}
+    if [[ $isolated == 1 ]]
+    then
+      INTERF="isolated"
+      echo "$TEST $INTERF"
+        
+      sudo echo 0 > /sys/kernel/debug/memguard/throttle
+      sleep 2
 
-  if [[ $isolated == 1 ]]
-  then
-    INTERF="isolated"
-    echo "$TEST $INTERF"
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+      sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
+      wait $PID_TO_WAIT
+
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
+
+      sleep 2
+
+      python3 splitftrace.py trace_${INTERF}.txt
+
+      sleep 10
+    fi
+
+
+    if [[ $memset == 1 ]]
+    then
+      INTERF="memset"
+      echo "$TEST against $INTERF"
+
+      sudo echo 0 > /sys/kernel/debug/memguard/throttle
+      sleep 2
+
+      sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -d $SIZE -s --iterations=20055 --mode=memset | grep "Memset BW" | awk '{ print $4 }'  >> bw_${INTERF}.txt & PID_TO_KILL0=$!
+      sleep 5
+
+      PID_TO_KILL=$(pgrep cudainterf)
+
+      sudo kill -s SIGUSR1 $PID_TO_KILL
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+      sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
+      wait $PID_TO_WAIT
+
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
+      sudo kill -s SIGUSR2 $PID_TO_KILL
+
+      wait $PID_TO_KILL0
+
+      sleep 2
+
+      sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
+      sudo killall -9 cudainterf > /dev/null 2>&1
+
+      python3 splitftrace.py trace_${INTERF}.txt
+
+      sleep 10
+    fi
+
+
+    if [[ $memcpy == 1 ]]
+    then
+      INTERF="memcpy"
+      echo "$TEST against $INTERF"
+
+      sudo echo 0 > /sys/kernel/debug/memguard/throttle
+      sleep 2
+
+      sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -d $SIZE -s --iterations=20055 --mode=memcpy | grep "Memcpy BW" | awk '{ print $4 }'  >> bw_${INTERF}.txt & PID_TO_KILL0=$!
+      sleep 5
+
+      PID_TO_KILL=$(pgrep cudainterf)
+
+      sudo kill -s SIGUSR1 $PID_TO_KILL
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+      sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
+      wait $PID_TO_WAIT
+
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
+      sudo kill -s SIGUSR2 $PID_TO_KILL
+
+      wait $PID_TO_KILL0
+
+      sleep 2
+
+      sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
+      sudo killall -9 cudainterf > /dev/null 2>&1
+
+      python3 splitftrace.py trace_${INTERF}.txt
+
+      sleep 10
+    fi
+
+
+    if [[ $bandwidth == 1 ]]
+    then
+      INTERF="bandwidth_read"
+      echo "$TEST against $INTERF"
+
+      sudo echo 0 > /sys/kernel/debug/memguard/throttle
+      sleep 2
+
+
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+
+      for c in 1 2 3; do bandwidth -c $c -t 1000 >> bw_${INTERF}.txt & done
+
+      taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
+
+      wait $PID_TO_WAIT
+      sudo killall -2 bandwidth > /dev/null 2>&1
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
+
+      sleep 1
+
+      sudo killall -9 bandwidth > /dev/null 2>&1
+
+      python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
+
+      sleep 5
+
+
+      INTERF="bandwidth_write"
+      echo "$TEST against $INTERF"
+
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+      for c in 1 2 3; do bandwidth -a write -c $c -t 1000 >> bw_${INTERF}.txt & done
+
+      taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
+
+      wait $PID_TO_WAIT
+      sudo killall -2 bandwidth > /dev/null 2>&1
+
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
+
+      sleep 1
+
+      sudo killall -9 bandwidth > /dev/null 2>&1
+
+      python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
+    fi
+
+
+    if [[ $bandwidth_heavy == 1 ]]
+    then
+      INTERF="bandwidth_read_memcpy"
+      echo "$TEST against $INTERF"
+
+      sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -s -d $SIZE -i 20000 -m memcpy | grep "Memcpy BW" | awk '{ print $4 }'  >> bw_${INTERF}_gpu.txt & PID_TO_KILL0=$!
+
+      sleep 5
+      PID_TO_KILL=$(pgrep cudainterf)
+
+      sudo kill -s SIGUSR1 $PID_TO_KILL
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+      for c in 1 2 3; do bandwidth -c $c -t 1000 >> bw_${INTERF}_cpu.txt & done
+
+      taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" > cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
+
+      wait $PID_TO_WAIT
+      sudo killall -2 bandwidth
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
+      sudo kill -s SIGUSR2 $PID_TO_KILL
+
+      sleep 1
+
+      wait $PID_TO_KILL0
+
+      sudo killall -9 bandwidth > /dev/null 2>&1
+      sudo kill -9 $PID_TO_KILL  > /dev/null 2>&1
+      sudo killall -9 cudainterf > /dev/null 2>&1
+
+      python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
+
+      sleep 10
+
+
+      INTERF="bandwidth_write_memset"
+      echo "$TEST against $INTERF"
+
+      sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -s -d $SIZE -i 20000 -m memset | grep "Memset BW" | awk '{ print $4 }'  >> bw_${INTERF}_gpu.txt & PID_TO_KILL0=$!
+
+      sleep 5
+      PID_TO_KILL=$(pgrep cudainterf)
+
+      sudo kill -s SIGUSR1 $PID_TO_KILL
+      sudo echo 0 > /sys/kernel/debug/tracing/trace
+      for c in 1 2 3; do bandwidth -a write -c $c -t 1000 >> bw_${INTERF}_cpu.txt & done 
+
+      taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" > cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
+
+      wait $PID_TO_WAIT
+      sudo killall -2 bandwidth
+      sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
+      sudo kill -s SIGUSR2 $PID_TO_KILL
+
+      sleep 1
+
+      wait $PID_TO_KILL0
       
-    sudo echo 0 > /sys/kernel/debug/memguard/throttle
-    sleep 2
+      sudo killall -9 bandwidth > /dev/null 2>&1
+      sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
+      sudo killall -9 cudainterf > /dev/null 2>&1
 
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
+      python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
+    fi
 
-    sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
-    wait $PID_TO_WAIT
+    mv *.txt res/${TEST}
 
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
-
-    sleep 2
-
-    python3 splitftrace.py trace_${INTERF}.txt
-
-    sleep 10
-  fi
-
-
-  if [[ $memset == 1 ]]
-  then
-    INTERF="memset"
-    echo "$TEST against $INTERF"
-
-    sudo echo 0 > /sys/kernel/debug/memguard/throttle
-    sleep 2
-
-    sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -d $SIZE -s --iterations=20055 --mode=memset | grep "Memset BW" | awk '{ print $4 }'  >> bw_${INTERF}.txt & PID_TO_KILL0=$!
-    sleep 5
-
-    PID_TO_KILL=$(pgrep cudainterf)
-
-    sudo kill -s SIGUSR1 $PID_TO_KILL
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-
-    sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
-    wait $PID_TO_WAIT
-
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
-    sudo kill -s SIGUSR2 $PID_TO_KILL
-
-    wait $PID_TO_KILL0
-
-    sleep 2
-
-    sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
-    sudo killall -9 cudainterf > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${INTERF}.txt
-
-    sleep 10
-  fi
-
-
-  if [[ $memcpy == 1 ]]
-  then
-    INTERF="memcpy"
-    echo "$TEST against $INTERF"
-
-    sudo echo 0 > /sys/kernel/debug/memguard/throttle
-    sleep 2
-
-    sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -d $SIZE -s --iterations=20055 --mode=memcpy | grep "Memcpy BW" | awk '{ print $4 }'  >> bw_${INTERF}.txt & PID_TO_KILL0=$!
-    sleep 5
-
-    PID_TO_KILL=$(pgrep cudainterf)
-
-    sudo kill -s SIGUSR1 $PID_TO_KILL
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-
-    sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${INTERF}.txt & PID_TO_WAIT=$! 
-    wait $PID_TO_WAIT
-
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${INTERF}.txt
-    sudo kill -s SIGUSR2 $PID_TO_KILL
-
-    wait $PID_TO_KILL0
-
-    sleep 2
-
-    sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
-    sudo killall -9 cudainterf > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${INTERF}.txt
-
-    sleep 10
-  fi
-
-
-  if [[ $bandwidth == 1 ]]
-  then
-    INTERF="bandwidth_read"
-    echo "$TEST against $INTERF"
-
-    sudo echo 0 > /sys/kernel/debug/memguard/throttle
-    sleep 2
-
-
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-
-    for c in 1 2 3; do bandwidth -c $c -t 1000 >> bw_${INTERF}.txt & done
-
-    taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
-
-    wait $PID_TO_WAIT
-    sudo killall -2 bandwidth > /dev/null 2>&1
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
-
-    sleep 1
-
-    sudo killall -9 bandwidth > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
-
-    sleep 5
-
-
-    INTERF="bandwidth_write"
-    echo "$TEST against $INTERF"
-
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-    for c in 1 2 3; do bandwidth -a write -c $c -t 1000 >> bw_${INTERF}.txt & done
-
-    taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
-
-    wait $PID_TO_WAIT
-    sudo killall -2 bandwidth > /dev/null 2>&1
-
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
-
-    sleep 1
-
-    sudo killall -9 bandwidth > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
-  fi
-
-
-  if [[ $bandwidth_heavy == 1 ]]
-  then
-    INTERF="bandwidth_read_memcpy"
-    echo "$TEST against $INTERF"
-
-    sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -s -d $SIZE -i 20000 -m memcpy | grep "Memcpy BW" | awk '{ print $4 }'  >> bw_${INTERF}_gpu.txt & PID_TO_KILL0=$!
-
-    sleep 5
-    PID_TO_KILL=$(pgrep cudainterf)
-
-    sudo kill -s SIGUSR1 $PID_TO_KILL
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-    for c in 1 2 3; do bandwidth -c $c -t 1000 >> bw_${INTERF}_cpu.txt & done
-
-    taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" > cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
-
-    wait $PID_TO_WAIT
-    sudo killall -2 bandwidth
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
-    sudo kill -s SIGUSR2 $PID_TO_KILL
-
-    sleep 1
-
-    wait $PID_TO_KILL0
-
-    sudo killall -9 bandwidth > /dev/null 2>&1
-    sudo kill -9 $PID_TO_KILL  > /dev/null 2>&1
-    sudo killall -9 cudainterf > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
-
-    sleep 10
-
-
-    INTERF="bandwidth_write_memset"
-    echo "$TEST against $INTERF"
-
-    sudo taskset -c 2 ../../benchmarks/hesoc-mark/cuda/cudainterf -s -d $SIZE -i 20000 -m memset | grep "Memset BW" | awk '{ print $4 }'  >> bw_${INTERF}_gpu.txt & PID_TO_KILL0=$!
-
-    sleep 5
-    PID_TO_KILL=$(pgrep cudainterf)
-
-    sudo kill -s SIGUSR1 $PID_TO_KILL
-    sudo echo 0 > /sys/kernel/debug/tracing/trace
-    for c in 1 2 3; do bandwidth -a write -c $c -t 1000 >> bw_${INTERF}_cpu.txt & done 
-
-    taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" > cycles_${TEST}_vs_${INTERF}.txt & PID_TO_WAIT=$!
-
-    wait $PID_TO_WAIT
-    sudo killall -2 bandwidth
-    sudo cat /sys/kernel/debug/tracing/trace > trace_${TEST}_vs_${INTERF}.txt 
-    sudo kill -s SIGUSR2 $PID_TO_KILL
-
-    sleep 1
-
-    wait $PID_TO_KILL0
-    
-    sudo killall -9 bandwidth > /dev/null 2>&1
-    sudo kill -9 $PID_TO_KILL > /dev/null 2>&1
-    sudo killall -9 cudainterf > /dev/null 2>&1
-
-    python3 splitftrace.py trace_${TEST}_vs_${INTERF}.txt
-  fi
-
-  mv *.txt res/${TEST}
-
-done
+  done
+fi
 
 sudo rmmod memguard
--- gen_tests/tests_dynamic/res/mser/bw_bandwidth_read_memcpy_cpu.txt
+++ gen_tests/tests_dynamic/res/mser/bw_bandwidth_read_memcpy_cpu.txt
@@ -1,15 +1,15 @@
 memsize=16384 KB, type=read, cpuid=1
 stop at 1000
 g_nread(bytes read) = 1577058304
-elapsed = 3.34 sec ( 3338303 usec )
-CPU1: B/W = 450.53 MB/s | CPU1: average = 135.47 ns
+elapsed = 3.35 sec ( 3345052 usec )
+CPU1: B/W = 449.62 MB/s | CPU1: average = 135.75 ns
 memsize=16384 KB, type=read, cpuid=2
 stop at 1000
-g_nread(bytes read) = 1577058304
-elapsed = 3.34 sec ( 3344304 usec )
-CPU2: B/W = 449.72 MB/s | CPU2: average = 135.72 ns
+g_nread(bytes read) = 1593835520
+elapsed = 3.34 sec ( 3344942 usec )
+CPU2: B/W = 454.42 MB/s | CPU2: average = 134.32 ns
 memsize=16384 KB, type=read, cpuid=3
 stop at 1000
-g_nread(bytes read) = 1577058304
-elapsed = 3.35 sec ( 3345197 usec )
-CPU3: B/W = 449.60 MB/s | CPU3: average = 135.75 ns
+g_nread(bytes read) = 1593835520
+elapsed = 3.35 sec ( 3345261 usec )
+CPU3: B/W = 454.37 MB/s | CPU3: average = 134.33 ns
