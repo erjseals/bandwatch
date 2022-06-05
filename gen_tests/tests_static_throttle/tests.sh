90e3ff7cbd02a7fea8ea31bb8c7ec7882730a5ef
Eric Seals <erjseals@gmail.com>
2022-06-05 11:20:19 -0500

    Rerunning for higher statistical certainty

--- gen_tests/tests_static_throttle/tests.sh
+++ gen_tests/tests_static_throttle/tests.sh
@@ -40,7 +40,6 @@ do
 
   for i in  "${THROTTLE_AMOUNT[@]}"
   do 
-    
 
     echo "Memset, Throttle Amount ${i}"
     sudo echo ${i} > /sys/kernel/debug/memguard/throttle
@@ -51,14 +50,15 @@ do
 
     PID_TO_KILL=$(pgrep cudainterf)
 
-    sudo kill -s SIGUSR1 $PID_TO_KILL
     sudo echo 0 > /sys/kernel/debug/tracing/trace
 
-    sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_memset.txt & PID_TO_WAIT=$! 
-    wait $PID_TO_WAIT
+    sudo kill -s SIGUSR1 $PID_TO_KILL
+    #sudo taskset -c 0 ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/${TEST} ../../benchmarks/sd-vbs/benchmarks/${TEST}/data/fullhd/. | grep "Cycles elapsed" >> cycles_memset.txt & PID_TO_WAIT=$! 
+    #wait $PID_TO_WAIT
+    sleep 5
+    sudo kill -s SIGUSR2 $PID_TO_KILL
 
     sudo cat /sys/kernel/debug/tracing/trace > trace_memset_$i.txt
-    sudo kill -s SIGUSR2 $PID_TO_KILL
 
     
     wait $PID_TO_KILL0
