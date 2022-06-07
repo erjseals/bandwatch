b5f4f27338fd984e9147b76fb4d16c6a13249ae5
Eric <erjseals@gmail.com>
2022-06-07 10:52:24 -0500

    Update for figure 13

--- gen_tests/plots/gen_histo.sh
+++ gen_tests/plots/gen_histo.sh
@@ -13,3 +13,7 @@ gnuplot -c histomemset.scr > plots_evaluation/bwmemset.pdf
 gnuplot -c histomemcpy.scr > plots_evaluation/bwmemcpy.pdf
 
 gnuplot -c histoCPUBW.scr > plots_evaluation/bwCPU.pdf
+
+gnuplot -c histogpu_CPUGPUBW.scr > plots_evaluation/bwgpu_CPUGPU.pdf
+
+gnuplot -c histocpu_CPUGPUBW.scr > plots_evaluation/bwcpu_CPUGPU.pdf
